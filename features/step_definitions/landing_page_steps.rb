25bb224ecaa9d37434f9f2f25ba6686e8cf64b1b
Erik <erik.steinman@hotmail.com>
2018-07-02 17:01:42 +0200

    Second test "I click "New article" link" green

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -1,13 +1,7 @@
 Given('I visit the "landing" page') do
     visit root_path
   end
-  
-  Then("I should see {string}") do |content|
-    expect(page).to have_content content
-  end
 
-  Given("the following articles exists") do |table|
-    table.hashes.each do |article|
-      Article.create!(article)
-    end
-end
+  When ('I click {string} link') do |string|
+    expect(page).to have_content string
+  end
