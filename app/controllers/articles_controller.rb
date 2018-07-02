66458e5c03df091852b8455a03f57ef68e01fc3c
Erik <erik.steinman@hotmail.com>
2018-07-02 20:21:34 +0200

    two new green tests

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,11 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  def new
+
+  end
+
+  def create
+    render plain: params[:article].inspect
+  end
 end
--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -2,6 +2,32 @@ Given('I visit the "landing" page') do
     visit root_path
   end
 
-  When ('I click {string} link') do |string|
-    expect(page).to have_content string
+  When ('I click {string} link') do |article_link|
+    click_link(article_link)
   end
+
+  When("I fill in {string} with {string}") do |field, content|
+    fill_in field, with: content
+ end
+  
+  When("I click {string} button") do |string|
+    click_button(string)
+    end
+  
+  Then("I should be on {string} page") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should see {string}.") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should see {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I shlould see {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  
